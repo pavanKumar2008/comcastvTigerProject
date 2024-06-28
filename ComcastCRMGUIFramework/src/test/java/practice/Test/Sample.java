2b085d2a53dc5e1d4b260f952df6b96e5d31b1a2
jyoti <jyoti@LAPTOP-LPR232VR>
2024-06-28 12:56:43 +0530

    test

--- ComcastCRMGUIFramework/src/test/java/practice/Test/Sample.java
+++ ComcastCRMGUIFramework/src/test/java/practice/Test/Sample.java
@@ -4,5 +4,7 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("Hii");
 	System.out.println("this is a test msg from id pavankumar2008");
+	int i=23;
+	System.out.println(i);
 }
 }
